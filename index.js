9fb18c85373991748a291337f330fffdf543db5b
szakeerfu <130739608+szakeerfu@users.noreply.github.com>
2023-04-15 15:17:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+(
+    function name() {
+
+        let screen = document.querySelector('.screen');
+        let buttons = document.querySelectorAll('.btn');
+        let equal = document.querySelector('.btn-equal');
+        let clear = document.querySelector('.btn-clear');
+
+        buttons.forEach((button) => {
+            button.addEventListener('click', function (e) {
+                let value = e.target.dataset.num;
+                screen.value += value
+
+
+            })
+
+        });
+
+        equal.addEventListener('click', function (e) {  
+            if (screen.value === '') {
+                screen.value = "please enter the number";
+
+            } else {
+                let answer = eval(screen.value);
+                screen.value = answer
+
+            }
+        })
+
+        clear.addEventListener('click', function(e){
+            screen.value = '';
+        })
+
+
+
+    })();
